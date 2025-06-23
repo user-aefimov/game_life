095948c21c4e618b3ec8a47dbe28feb2551db4b6
user-aefimov <user.a.efimov@gmail.com>
2025-06-23 17:39:17 +0300

    Correction of the is_game_over method

--- game_of_life.py
+++ game_of_life.py
@@ -60,46 +60,47 @@ class GameOfLife(metaclass=SingletonMeta):
     def generate_universe(self):
         return [[1 if random.random() > 0.7 else 0 for _ in range(self.__width)] for _ in range(self.__height)]
 
-@staticmethod
-def __get_near(universe, pos):
-    """Подсчитывает количество живых соседей с учетом торической геометрии"""
-    if not universe or not universe[0]:
-        return 0  # Защита от пустой вселенной
+    @staticmethod
+    def __get_near(universe, pos):
+        """Подсчитывает количество живых соседей с учетом торической геометрии"""
+        if not universe or not universe[0]:
+            return 0  # Защита от пустой вселенной
     
-    height = len(universe)
-    width = len(universe[0])
-    count = 0
-    
-    # Все 8 возможных направлений
-    directions = [(-1,-1), (-1,0), (-1,1),
-                 (0,-1),           (0,1),
-                 (1,-1),  (1,0),   (1,1)]
-    
-    for dx, dy in directions:
-        # Торические координаты (зацикленный мир)
-        x = (pos[0] + dx) % height
-        y = (pos[1] + dy) % width
+        height = len(universe)
+        width = len(universe[0])
+        count = 0
         
-        if universe[x][y]:
-            count += 1
+        # Все 8 возможных направлений
+        directions = [(-1,-1), (-1,0), (-1,1),
+                    (0,-1),           (0,1),
+                    (1,-1),  (1,0),   (1,1)]
+        
+        for dx, dy in directions:
+            # Торические координаты (зацикленный мир)
+            x = (pos[0] + dx) % height
+            y = (pos[1] + dy) % width
             
-    return count
+            if universe[x][y]:
+                count += 1
+                
+        return count
     
     def is_game_over(self):
+        """Проверяет условия завершения игры с оптимизациями"""
         # Пропускаем проверку на 0-м поколении
         if self.generation == 0:
             return None
-        """Проверяет условия завершения игры"""
-        # Условие 1: все клетки мертвы
+        
+        # Условие 1: все клетки мертвы (оптимизировано)
         if self.is_all_dead():
             return "Все клетки вымерли! Игра окончена."
         
-        # Условие 2: мир стабилизировался (не изменяется)
-        if self.world == self.old_world:
+        # Условие 2: мир стабилизировался (не изменяется) с защитой от ранних срабатываний
+        if self.generation > 3 and self.world == self.old_world:
             return "Мир стабилизировался! Игра окончена."
         
         # Условие 3: достигнуто максимальное число поколений
-        if self.generation >= 100:  # Можно настроить
+        if self.generation >= 100:
             return f"Достигнут предел в {self.generation} поколений!"
         
         return None
